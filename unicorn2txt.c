56c39af8965f84feadeb35c338b88a92e0129632
Robert Oostenveld <r.oostenveld@gmail.com>
2022-10-11 23:06:45 +0200

    added description and copyrights to the top of the code

--- unicorn2txt.c
+++ unicorn2txt.c
@@ -1,3 +1,25 @@
+/*
+ * This application reads EEG data from the Unicorn from a serial-over-bluetooth device
+ * and writes it to screen, or to a tab-separated ascii file.
+ *
+ * Copyright (C) 2022, Robert Oostenveld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -119,7 +141,7 @@ int main(int argc, char **argv)
         }
 
         signal(SIGINT, signal_handler);
-        
+
         fprintf(fp, "eeg1\teeg2\teeg3\teeg4\teeg5\teeg6\teeg7\teeg8\taccel1\taccel2\taccel3\tgyro1\tgyro2\tgyro3\tbattery\tcounter\n");
 
         while (running) {
